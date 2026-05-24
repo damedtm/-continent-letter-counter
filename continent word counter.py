815e36114f223708cd1ed869fc126150cd327ee4
Damian Ohajunwa <damianohajunwa88@gmail.com>
2026-05-24 15:01:46 -0500

    Implemented logic for web scraping and evaluating the most recurring letter. Made the code more user-friendly by adding links to supplemental websites about continents

--- continent word counter.py
+++ continent word counter.py
@@ -1,10 +1,20 @@
-#Program to calculate the most recurring word in the countries of a continent.
+#Program to calculate the most recurring letters in the countries of a continent.
 #url: https://ontheworldmap.com/countries/by-continents/#
 
 import webbrowser
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+from collections import Counter
+
+natGeoAddress = 'https://education.nationalgeographic.org/resource/Continent/'
+anchorAddress = 'https://ontheworldmap.com/countries/by-continents/#'
+continentsList =[ 'asia', 'europe', 'africa', 'north america',  
+                  'south america', 'australia', 'oceania',  
+                  'australia and oceania', 'antartica']
+countriesListNormalized = []
+countriesNormalizedandJoined =[]
+countriesList = []
 
 def get_browser_headers():
     ua = UserAgent()
@@ -12,7 +22,7 @@ def get_browser_headers():
         'User-Agent': ua.random,
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.9',
-        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Encoding': 'identity',
         'Connection': 'keep-alive',
         'Upgrade-Insecure-Requests': '1',
         'Sec-Fetch-Dest': 'document',
@@ -20,51 +30,41 @@ def get_browser_headers():
         'Sec-Fetch-Site': 'none',
         'Cache-Control': 'max-age=0',
     }
-anchorAddress = 'https://ontheworldmap.com/countries/by-continents/#'
 
+print("Enter any continent to find out the most recurring letter in its names of its countries")
+userContinent = input()
+normalizedUserContinent = userContinent.lower()
 
-'''continentsDict = {'Asia' : 'as', 'Europe' : 'eu', 'Africa' : 'af', 'Oceania' : 'au', 'South America' : 'sa',
-                  'North America' : 'na', 'Antartica' : 'an'}'''
+if normalizedUserContinent == 'australia':
+        normalizedUserContinent = 'oceania'
 
-continentsList =[ 'asia', 'europe', 'africa', 'north america',  
-                  'south america', 'australia', 'oceania',  
-                  'australia and oceania', 'antartica']
-alphabetsList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
-                 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
-                 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-countriesList = []
-normalizedCountriesList =[]
-userContinent = []
-
-print('Enter any continent to find out the most recurring letter in its names of its countries')
-userContinent.append(input())
-if userContinent[0] == 'australia':
-        userContinent[0] = 'oceania'
-
-normalizedUserContinent = userContinent[0].lower()
-
-if (len(userContinent[0]) > 1) and userContinent[0] in continentsList:
-
+if  normalizedUserContinent in continentsList:
     headers = get_browser_headers()
-    webAddress = anchorAddress + normalizedUserContinent[0].replace(' ', '-') 
+    webAddress = anchorAddress + normalizedUserContinent.replace(' ', '-') 
     res = requests.get(webAddress, headers = headers)
     res.raise_for_status()
     print(res.status_code)
-    soup = BeautifulSoup(res.content, 'lxml')
-   
-    header = soup.find('h2', string=userContinent[0].title())
-    countries = []
-    for sibling in header.find_all_next():
+    soup = BeautifulSoup(res.text, 'lxml')
+    
+    continentHeading = soup.find('h2', string = normalizedUserContinent.title())
+    for sibling in continentHeading.find_all_next():
         if sibling.name == 'h2':
             break
         if sibling.name == 'ul' and 'ul-reset' in sibling.get('class', []):
             for li in sibling.find_all('li'):
-                countries.append(li.get_text())
+                countriesListNormalized.append(li.find('a').get_text().replace(" ",""))
+                countriesList.append(li.find('a').get_text())
+    
+    countriesNormalizedandJoined = "".join(countriesListNormalized)
+    count = Counter(countriesNormalizedandJoined)
+    letter = count.most_common(1)
+    topLetter, topCount = count.most_common(1)[0]
 
-    print(countries)
+    print(f"The most recurring letter in {normalizedUserContinent.title()} is '{topLetter}', occurring {topCount} times!")
+    print(f"You can follow this link to view interesting facts about the Continents: {natGeoAddress}")
 
 else:
-    print('Program failed')
+    print(f"Invalid input detected, please enter a continent e.g. 'Africa'")
 
 
 
