9b61172fb182ba0fa4475861cac64429073945ed
Damian Ohajunwa <damianohajunwa88@gmail.com>
2026-05-25 13:42:33 -0500

    ix: scope sibling traversal to prevent sidebar bleed and handle Antarctica edge case

--- continent word counter.py
+++ continent word counter.py
@@ -1,7 +1,7 @@
 #Program to calculate the most recurring letters in the countries of a continent.
 #url: https://ontheworldmap.com/countries/by-continents/#
 
-import webbrowser
+import sys
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
@@ -15,7 +15,7 @@ continentsList =[ 'asia', 'europe', 'africa', 'north america',
 countriesListNormalized = []
 countriesNormalizedandJoined =[]
 countriesList = []
-
+antarcticaLetterCount = 'A'
 def get_browser_headers():
     ua = UserAgent()
     return {
@@ -34,6 +34,12 @@ def get_browser_headers():
 print("Enter any continent to find out the most recurring letter in its names of its countries")
 userContinent = input()
 normalizedUserContinent = userContinent.lower()
+if normalizedUserContinent == 'antarctica':
+    print(f"The most recurring letter in {normalizedUserContinent.title()} is '{antarcticaLetterCount}', occurring 4 times!")
+    print(f"You can follow this link to view interesting facts about the Continents: {natGeoAddress}")
+    sys.exit()
+
+        
 
 if normalizedUserContinent == 'australia':
         normalizedUserContinent = 'oceania'
@@ -46,22 +52,35 @@ if  normalizedUserContinent in continentsList:
     print(res.status_code)
     soup = BeautifulSoup(res.text, 'lxml')
     
-    continentHeading = soup.find('h2', string = normalizedUserContinent.title())
-    for sibling in continentHeading.find_all_next():
+
+    countries_section = soup.find('section', class_='countries')
+    continentHeading = countries_section.find('h2', string=normalizedUserContinent.title())
+
+    for sibling in continentHeading.next_siblings:
         if sibling.name == 'h2':
             break
-        if sibling.name == 'ul' and 'ul-reset' in sibling.get('class', []):
-            for li in sibling.find_all('li'):
-                countriesListNormalized.append(li.find('a').get_text().replace(" ",""))
-                countriesList.append(li.find('a').get_text())
-    
+        if sibling.name == 'div':
+            for ul in sibling.find_all('ul', class_='ul-reset'):
+                for li in ul.find_all('li'):
+                    a = li.find('a')
+                    if a:
+                        countriesListNormalized.append(a.get_text().replace(" ", ""))
+                        countriesList.append(a.get_text())
+
     countriesNormalizedandJoined = "".join(countriesListNormalized)
     count = Counter(countriesNormalizedandJoined)
     letter = count.most_common(1)
     topLetter, topCount = count.most_common(1)[0]
+    total = 0
 
-    print(f"The most recurring letter in {normalizedUserContinent.title()} is '{topLetter}', occurring {topCount} times!")
-    print(f"You can follow this link to view interesting facts about the Continents: {natGeoAddress}")
+    print(f"The most recurring letter in {normalizedUserContinent.title()} is '{topLetter.title()}', occurring {topCount} times!\n")
+    print(f"You can follow this link to view interesting facts about the Continents: {natGeoAddress}\n")
+    for i in countriesList:
+        total = total + 1
+    
+    print(f"Here are the {total} countries included:")
+    for i in countriesList:
+        print (f"    {i}")
 
 else:
     print(f"Invalid input detected, please enter a continent e.g. 'Africa'")
